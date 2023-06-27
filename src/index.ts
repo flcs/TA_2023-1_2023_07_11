172a4bda6161d0fa1050a6feacdbb754d581dfaa
Fernando Carvalho <fernando.carvalho.iff@gmail.com>
2023-06-27 20:18:53 -0300

    removendo código duplicado

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,6 @@
 import express, { Request, Response } from "express";
 import { cria_rota } from "./rotas/rota";
 
-interface IUseCase {
-    perform() : string;
-}
-
-interface IRepositorio {
-    get(): string;
-}
-
-class Repositorio implements IRepositorio {
-    get() {
-        console.log('repositorio')
-        return 'repositorio';
-    }
-}
-
-class UseCase implements IUseCase {
-    repo: IRepositorio;
-    constructor(repo: IRepositorio) {
-        this.repo = repo;
-    }
-    perform() {
-        console.log('use case')
-        return this.repo.get();
-    }
-}
-
-class Contorlador {
-    usecase: IUseCase;
-    constructor(usecase: IUseCase) {
-        this.usecase = usecase;
-    }
-
-    handler(req: Request, resp: Response) {
-        console.log('chegou requisição')
-        const retorno = this.usecase.perform();
-        resp.json({ message: retorno}).end();
-    }
-}
-
-function Fabrica_De_Controladores() {
-
-    const repositorio = new Repositorio();
-    const usecase = new UseCase(repositorio);
-    const controlador = new Contorlador(usecase);
-    return controlador;
-}
-
-function cria_rota() {
-
-    const router = express.Router();
-    const controlador = Fabrica_De_Controladores();
-    router.get("/", controlador.handler.bind(controlador));
-    return router;
-}
-
 
 const app = express();
 
