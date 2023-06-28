54e95e42851b1699b07fbbc244ea8840b18c7a7c
Fernando Carvalho <fernando.carvalho.iff@gmail.com>
2023-06-27 21:28:50 -0300

    criando fábrica do SUT

--- src/domain/usecases/usecase.test.ts
+++ src/domain/usecases/usecase.test.ts
@@ -9,14 +9,21 @@ class RepoStub implements IRepositorio {
     }
 }
 
+function cria_sut() {
+    const repo_fake = new RepoStub();
+    const sut = new UseCase(repo_fake);
+    return { repo_fake, sut };
+}
+
 describe('UseCase exemplo', () => {
 
     it('should create a usecase', () => {
         // arragen
-        const repo_fake = new RepoStub();
-        const usecase = new UseCase(repo_fake);
+        // const repo_fake = new RepoStub();
+        // const sut = new UseCase(repo_fake);
+        const { sut, repo_fake } = cria_sut();
         // act
-        const result = usecase.perform({id : '10000'});
+        const result = sut.perform({id : '10000'});
 
         // assert
         expect(result).toBe('repo fake');
@@ -25,14 +32,14 @@ describe('UseCase exemplo', () => {
     it('deve levantar uma excessão quando o id for -1', () => {
         // arragen
         const repo_fake = new RepoStub();
-        const usecase = new UseCase(repo_fake);
+        const sut = new UseCase(repo_fake);
         // act
         // const result = usecase.perform({id : '-1'});
 
         // assert
         // expect(result).toEqual(new Error('id inválido'));
         expect( () => 
-            usecase.perform({id : '-1'})
+            sut.perform({id : '-1'})
         )
         .toThrow(Error('id inválido'));
     });
